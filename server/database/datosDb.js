10b5548e303f1dcdd12a104733632f9aa616645a
DJRICHARD15 <richardj150314@gmail.com>
2019-08-08 12:36:20 -0500

    csRichard

--- server/database/datosDb.js
+++ server/database/datosDb.js
@@ -1,10 +1,10 @@
 ;
-module.exports =  {
+module.exports = {
     client: process.env.CLIENT || 'pg',
-    connection: process.env.CONNECTION_DB || {  
-                                                host: '127.0.0.1', 
-                                                user: 'jarojas', 
-                                                password: 'juan123', 
-                                                database: 'Ferre'
-                                            }
+    connection: process.env.CONNECTION_DB || {
+        host: '127.0.0.1',
+        user: 'postgres',
+        password: 'lenin1234',
+        database: 'Ferre'
+    }
 }
--- server/instalacion/configuracion/entorno.js
+++ server/instalacion/configuracion/entorno.js
@@ -3,13 +3,13 @@ let initVar = () => {
     process.env.PORT = process.env.PORT || 8001
     process.env.CLIENT = process.env.CLIENT || 'pg'
     process.env.CONNECTION_DB = process.env.CONNECTION_DB || {
-                                                                host: '127.0.0.1', 
-                                                                user: 'jarojas', 
-                                                                password: 'juan123', 
-                                                                database: 'Ferre'
-                                                              }
-  }
-  
-  module.exports = {
+        host: '127.0.0.1',
+        user: 'postgres',
+        password: 'lenin1234',
+        database: 'Ferre'
+    }
+}
+
+module.exports = {
     initVar
-  }
+}
